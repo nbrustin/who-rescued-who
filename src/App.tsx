eb27386d525381cd6b3d33646e9f634585ce8f87
Nick Brustin <nick@absnetwork.com>
2022-10-02 22:05:16 -0700

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <Container>
         <Header />
         <Routes>
-          <Route path="/" element={<Search />} />
+          <Route path="/who-rescued-who" element={<Search />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </Container>
